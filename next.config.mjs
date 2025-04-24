fd98fd6af833c60503ef23d3166850bb8594d479
Sr Fearless <tiagofreitasmachado@hotmail.com>
2025-04-24 12:26:04 -0300

    Page no Github

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
+/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
+export default {
   output: isProd ? 'export' : undefined,
   basePath: isProd ? '/Portifolio-Pessoal' : '',
   assetPrefix: isProd ? '/Portifolio-Pessoal/' : '',
